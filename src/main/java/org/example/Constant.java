210e835185fe98748fc7aa5612ebcea92a5b9238
JudeJoseph10 <66405002+JudeJoseph10@users.noreply.github.com>
2022-11-06 22:02:39 +0530

    update the constant file

--- src/main/java/org/example/Constant.java
+++ src/main/java/org/example/Constant.java
@@ -3,4 +3,5 @@ package org.example;
 public class Constant {
 
     public static String textMessage = "Welcome to Git Demo Project ...";
+    public static String updateMessage = "Update the constant file ...";
 }
