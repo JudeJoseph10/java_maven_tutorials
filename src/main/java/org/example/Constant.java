7d3f0a66ea0345a479ff85753ac8f3308cf5ee3c
JudeJoseph10 <66405002+JudeJoseph10@users.noreply.github.com>
2022-11-07 21:59:52 +0530

    Add a constant
    Add a constant

--- src/main/java/org/example/Constant.java
+++ src/main/java/org/example/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
 
     public static String textMessage = "Welcome to Git Demo Project ...";
     public static String updateMessage = "Update the constant file ...";
+    public static String textfromgithub = "Github Changes ....";
 }
